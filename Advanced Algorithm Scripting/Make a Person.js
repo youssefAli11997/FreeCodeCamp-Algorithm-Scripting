ec21792db50a313c8fe15b4909291d1fbb62fae3
Youssef Ali <youssefali6212@gmail.com>
2018-01-11 12:58:54 +0200

    Update Make a Person.js

--- Advanced Algorithm Scripting/Make a Person.js
+++ Advanced Algorithm Scripting/Make a Person.js
@@ -1,8 +1,8 @@
 //link: https://www.freecodecamp.org/challenges/make-a-person
 var Person = function(firstAndLast) {
-    // Complete the method below and implement the others similarly
     firstName = firstAndLast.split(" ")[0];
     lastName = firstAndLast.split(" ")[1];
+    // Complete the method below and implement the others similarly
     this.getFullName = function() {
       return firstName + " " + lastName;
     };
